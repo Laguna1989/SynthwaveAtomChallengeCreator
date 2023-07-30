cb8b707a5ea417971273ca1b1fc5e82fc79f27f4
Simon Weis <laguna.ws@gmail.com>
2023-07-30 09:13:24 +0200

    Split into files, add more modifirs

---
+++ drums.py
@@ -0,0 +1,30 @@
+import random
+
+
+def get_random_drums():
+    drums_array = [
+        "E-mu Drumulator",
+
+        "Korg DDD-1",
+
+        "Linn 9000",
+        "Linn LinnDrum",
+        "Linn LM-1"
+
+        "Oberheim DMX",
+
+        "Roland CR76",
+        "Roland R8",
+        "Roland TR-626",
+        "Roland TR-707",
+        "Roland TR-808",
+        "Roland TR-909",
+
+        "SCI DrumTraks",
+
+        "Simmons SDSV",
+
+        "Yamaha RX5",
+        "Yamaha RY30"
+    ]
+    return random.choice(drums_array)
---
+++ timings.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_random_tempo():
+    return str(random.randint(75, 140))
+
+
+def get_timing():
+    return "4/4"
+
+
+def get_track_duration():
+    return "60-90s"
