30414348441ef4d597237f4301880652a1789d6a
Simon Weis <laguna.ws@gmail.com>
2023-12-26 06:29:24 +0100

    Add bash file to restart docker

---
+++ restart_docker_container.sh
@@ -0,0 +1,15 @@
+echo "pull latest version of the SynthwaveAtomChallengeCreator repository"
+git reset --hard
+git checkout master
+git pull
+
+echo ""
+
+echo "pull latest docker image"
+docker pull ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
+
+DOCKER_CONTAINER_ID=`docker ps | grep ghcr.io/laguna1989/synthwaveatomchallengecreator:latest | awk '{print $1}' | tail -1`
+echo ""
+echo "attempting to restart container with id $DOCKER_CONTAINER_ID"
+
+docker restart $DOCKER_CONTAINER_ID
