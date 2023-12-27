9f2c7b9cbf0bb586e87da57fd378f82206207027
Simon Weis <laguna.ws@gmail.com>
2023-12-27 14:40:21 +0100

    Only restart docker container if there is a new image

--- restart_docker_container.sh
+++ restart_docker_container.sh
@@ -12,15 +12,36 @@ git pull
 
 echo ""
 
+OLD_IMAGE_ID =`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator:latest | grep latest | awk '{print $3}'`
+
 echo "pull latest docker image"
 docker pull ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
 
-DOCKER_CONTAINER_ID=`docker ps | grep ghcr.io_laguna1989_synthwaveatomchallengecreator_latest | awk '{print $1}' | tail -1`
 echo ""
-echo "attempting to restart container"
-echo "stop container with id $DOCKER_CONTAINER_ID"
-docker stop $DOCKER_CONTAINER_ID
 
-echo ""
-echo "start new docker container"
-docker run --restart=always --env-file .env -d --name "ghcr.io_laguna1989_synthwaveatomchallengecreator_latest" ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
+UPDATED_IMAGE_ID =`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator:latest | grep latest | awk '{print $3}'`
+
+
+echo "old container image id: $OLD_IMAGE_ID"
+echo "new container image id: $UPDATED_IMAGE_ID"
+
+if [ "$UPDATED_IMAGE_ID" = "$OLD_IMAGE_ID" ]; then
+  echo "image ids differ, restart docker"
+
+  DOCKER_CONTAINER_ID=`docker ps | grep ghcr.io_laguna1989_synthwaveatomchallengecreator_latest | awk '{print $1}' | tail -1`
+  echo ""
+  echo "attempting to restart container"
+  echo "stop container with id $DOCKER_CONTAINER_ID"
+  docker stop $DOCKER_CONTAINER_ID
+
+  echo ""
+  echo "remove docker container"
+  docker container remove $DOCKER_CONTAINER_ID
+
+  echo ""
+  echo "start new docker container"
+  docker run --restart=always --env-file .env -d --name "ghcr.io_laguna1989_synthwaveatomchallengecreator_latest" ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
+else
+  echo "no new image, keep docker running"
+fi
+
