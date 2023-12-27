aea4d2666e2325125cfb8ec354d23510fdec1682
Simon Weis <laguna.ws@gmail.com>
2023-12-27 16:28:39 +0100

    correctly assign bash variables

--- restart_docker_container.sh
+++ restart_docker_container.sh
@@ -12,20 +12,20 @@ git pull
 
 echo ""
 
-OLD_IMAGE_ID =`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator | grep latest | awk '{print $3}'`
+OLD_IMAGE_ID=`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator | grep latest | awk '{print $3}'`
 
 echo "pull latest docker image"
 docker pull ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
 
 echo ""
 
-UPDATED_IMAGE_ID =`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator | grep latest | awk '{print $3}'`
+UPDATED_IMAGE_ID=`docker image list | grep ghcr.io/laguna1989/synthwaveatomchallengecreator | grep latest | awk '{print $3}'`
 
 
 echo "old container image id: $OLD_IMAGE_ID"
 echo "new container image id: $UPDATED_IMAGE_ID"
 
-if [ "$UPDATED_IMAGE_ID" = "$OLD_IMAGE_ID" ]; then
+if [[ "$OLD_IMAGE_ID" == "UPDATED_IMAGE_ID" ]]; then
   echo "image ids differ, restart docker"
 
   DOCKER_CONTAINER_ID=`docker ps | grep ghcr.io_laguna1989_synthwaveatomchallengecreator_latest | awk '{print $1}' | tail -1`
@@ -40,8 +40,7 @@ if [ "$UPDATED_IMAGE_ID" = "$OLD_IMAGE_ID" ]; then
 
   echo ""
   echo "start new docker container"
-  docker run --restart=always --env-file .env -d --name "ghcr.io_laguna1989_synthwaveatomchallengecreator_latest" ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
+  docker run --restart=always --env-file ~/.env -d --name "ghcr.io_laguna1989_synthwaveatomchallengecreator_latest" ghcr.io/laguna1989/synthwaveatomchallengecreator:latest
 else
   echo "no new image, keep docker running"
 fi
-
